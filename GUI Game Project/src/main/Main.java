bfe614051b2c7081130c14ce98739dab081e3e20
titoi <titoi@PS5.frontier.com>
2022-05-23 16:40:23 -0400

    v 0.1

--- GUI Game Project/src/main/Main.java
+++ GUI Game Project/src/main/Main.java
@@ -1,7 +1,10 @@
 package main;
-
+import javax.swing.*;
+import java.awt.*;
 public class Main {
 	public static void main(String[] args) {
-		System.out.print("Hello Titokolop");
+		JFrame Start = new JFrame("BlockMan Adventures Launcher");
+		Start.setSize(500,500);
+		Start.setVisible(true);
 	}
 }
