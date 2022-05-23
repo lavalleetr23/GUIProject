b2d074e66c0563dc6a45ed61288b5defd2da385b
titoi <titoi@PS5.frontier.com>
2022-05-23 16:46:21 -0400

    Red Panel

--- GUI Game Project/src/main/Main.java
+++ GUI Game Project/src/main/Main.java
@@ -6,5 +6,10 @@ public class Main {
 		JFrame Start = new JFrame("BlockMan Adventures Launcher");
 		Start.setSize(700,700);
 		Start.setVisible(true);
+		JPanel StartPanel = new JPanel();
+		StartPanel.setBackground(Color.RED);
+		StartPanel.setBounds(0,0,700,700);
+		StartPanel.setVisible(true);
+		Start.add(StartPanel);
 	}
 }
