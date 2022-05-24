a7860f225c575f1771237aba8338073ad232f9f6
lavalleetr23 <lavalleetr23@L2023-0598.amityregion5.org>
2022-05-24 08:16:10 -0400

    Broken Button

--- GUI Game Project/src/main/Main.java
+++ GUI Game Project/src/main/Main.java
@@ -7,9 +7,19 @@ public class Main {
 		Start.setSize(700,700);
 		Start.setVisible(true);
 		JPanel StartPanel = new JPanel();
-		StartPanel.setBackground(Color.RED);
+		StartPanel.setBackground(Color.GREEN);
 		StartPanel.setBounds(0,0,700,700);
 		StartPanel.setVisible(true);
 		Start.add(StartPanel);
+		JLabel Title = new JLabel("BlockMan Adventures");
+		StartPanel.add(Title);
+		StartPanel.validate();
+		Title.setBounds(250, 50, 200, 100);
+		Title.setFont(new Font("Serif", Font.BOLD, 50));
+		Title.setVisible(true);
+		JButton Play = new JButton("Play");
+		Start.add(Play);
+		Play.setBounds(300, 550, 100, 50);
+		Play.setVisible(true);
 	}
 }
