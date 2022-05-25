29b5d75b5a2c9ba837c89e7680d40f7d8c0ed22d
Yazeed <yazeed313233@gmail.com>
2022-05-24 21:39:15 -0400

    The big stuff

--- GUI Game Project/src/main/Level1Graphics.java
+++ GUI Game Project/src/main/Level1Graphics.java
@@ -1,24 +1,107 @@
 package main;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.*;
-public class Level1Graphics extends Canvas {
+
+
+public class Level1Graphics extends Panel implements ActionListener, KeyListener {
+	//Timer stuff
+	private Timer timer;
+	private int delay = 8;
+
+	//Block movement variables
+	private int blockX = 320;
+	private int blockY = 540;
+
 	public void paint(Graphics g) {
-		for(int i=0;i<300;i++) {
-			int x = (int) (Math.random()*600);
-			int y = (int) (Math.random()*600);
-			g.fillOval(x, y, 30, 30);
-			if(x>0) {
-				setForeground(Color.BLUE);
-			}
-			if(x>100) {
-				setForeground(Color.MAGENTA);
-			}
-			if(x>300) {
-				setForeground(Color.CYAN);
-			}
-			if(x>500) {
-			setForeground(Color.GREEN);
-			}
+		//background
+		setBackground(Color.CYAN);
+
+		//Ground
+		g.setColor(Color.GREEN);
+		g.fillRect(0,600,700,440);
+
+		//BlockMan
+		g.setColor(Color.gray);
+		g.fillRect(blockX,blockY,60,60);
+
+		//Door Platform
+		g.setColor(Color.LIGHT_GRAY);
+		g.fillRect(275,200,200,30);
+
+		//Door
+		g.setColor(Color.black);
+		g.fillRect(325,120,100,80);
+		g.fillArc(325,100,100,40,180,-180);
+
+
+		g.setColor(Color.lightGray);
+
+		//Borders
+		g.fillRect(0,0,700,10);
+		g.fillRect(0,0,10,700);
+		g.fillRect(700,0,10,700);
+		g.fillRect(0,700,700,10);
+
+		//Platforms
+		g.fillRect(40,350,200,30);
+		g.fillRect(400,490,200,30);
+
+		//Coins
+		g.setColor(Color.yellow);
+		g.fillOval(120,290,50,50);
+		g.fillOval(480,430,50,50);
+
+	}
+	//Its what looks for key inputs
+	public Level1Graphics(){
+		addKeyListener(this);
+		timer = new Timer(delay,this);
+		timer.start();
+	}
+
+	//This happens whenever a key or a sort of action is performed
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//Basically the block moves since as you hold the button it keeps doing this and repainting the Gui
+		repaint();
+	}
+
+	//This is for clicks
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	//This is when you hold the key
+	@Override
+	public void keyPressed(KeyEvent e) {
+		//Increments left each second or something
+		if (e.getKeyCode() == KeyEvent.VK_LEFT){
+			blockX = blockX - 10;
 		}
+
+		//Increments right each second or something
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+			blockX = blockX + 10;
+		}
+
+		//These two if statements set the boundaries for how far left or right it can go
+		if (blockX < 10){
+			blockX = 10;
+		}
+		if (blockX > 640){
+			blockX = 640;
+		}
+
+	}
+
+	//In the name ngl
+	@Override
+	public void keyReleased(KeyEvent e) {
+
 	}
 }
--- GUI Game Project/src/main/Main.java
+++ GUI Game Project/src/main/Main.java
@@ -3,23 +3,37 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 public class Main {
 	public static void main(String[] args) {
+
+		//Sets starting frame
 		JFrame Start = new JFrame("BlockMan Adventures Launcher");
 		Start.setSize(700,700);
 		Start.setVisible(true);
+
+		//Sets the main page panel
 		JPanel StartPanel = new JPanel();
 		StartPanel.setBackground(Color.lightGray);
 		StartPanel.setBounds(0,0,700,700);
 		StartPanel.setVisible(true);
 		Start.add(StartPanel);
+
+		//Sets label for start page
 		JLabel Title = new JLabel("BlockMan Adventures");
 		StartPanel.add(Title);
 		Title.setBounds(250, 50, 200, 100);
 		Title.setFont(new Font("Serif", Font.BOLD, 50));
 		Title.setVisible(true);
+
+		//Creates and adds button
 		JButton Play = new JButton("Play");
-		 Play.addActionListener(new ActionListener(){  
+		Start.add(Play);
+		Play.setBounds(300, 550, 100, 50);
+		Play.setVisible(true);
+
+		//Adds Action Listener to the Play button, opens new panel when clicked
+		Play.addActionListener(new ActionListener(){
 			 public void actionPerformed(ActionEvent e){  
 			           JFrame Game = new JFrame("BlockMan Adventures"); 
 			           Game.setSize(700,700);
@@ -28,9 +42,8 @@ public class Main {
 			           Level1Graphics L1 = new Level1Graphics();
 			           Game.add(L1);
 			         }  
-			     });  
-		Start.add(Play);
-		Play.setBounds(300, 550, 100, 50);
-		Play.setVisible(true);
+		});
+
+
 	}
 }
